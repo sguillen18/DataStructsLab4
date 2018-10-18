86fde95e00d36176d8acc11c0ccf830a1adf5cbe
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-10-18 16:13:49 -0400

    Add files via upload
    boop

--- AList.java
+++ AList.java
@@ -1,34 +1,37 @@
-package Lab4;
 import java.util.Arrays;
 
-public class AList<T> implements ListInterface <T>   {
+public class AList <T> implements ListInterface <T>   {
     private T[] list;
     private int numberOfEntries;
     private static final int DEFAULT_CAPACITY = 10;
     private static final int MAX_CAPACITY = 10000;
     private boolean initialized = false;
     
-    public AList( int capacity) {
+    //constructors
+    public AList () {
+        this(DEFAULT_CAPACITY);
+    }
+    
+    public AList(int capacity) {
         if (capacity < DEFAULT_CAPACITY)
             capacity = DEFAULT_CAPACITY;
         else
-            checkCapacity (capacity);
-        T[] temp = (T[]) new Object[capacity];
+            ensureCapacity();
+        @SuppressWarnings("unchecked")
+		T[] temp = (T[]) new Object[capacity];
         list = temp;
         numberOfEntries = 0;
         initialized = true;
     }
     
-    private void checkCapacity(int num) {
-    	if(num > list.length) {
-    	//
+    //check initialization
+    private void checkInitialization() {
+    	if(!initialized) {
+    		throw new SecurityException();
     	}
     }
     
-    public AList () {
-        this(DEFAULT_CAPACITY);
-    }
-    
+    //method declarations
     @Override
     public void add (T anEntry) {
       checkInitialization();
@@ -37,10 +40,10 @@ public class AList<T> implements ListInterface <T>   {
       ensureCapacity();
     }
     
-    @Override
     public T[] toArray() {
         checkInitialization();
-        T[] result = (T[]) new Object[numberOfEntries];
+        @SuppressWarnings("unchecked")
+		T[] result = (T[]) new Object[numberOfEntries];
         for (int idx = 0; idx < numberOfEntries; idx ++)
             result[idx] = list[idx];
         return result;
@@ -50,48 +53,60 @@ public class AList<T> implements ListInterface <T>   {
         int capacity = list.length - 1; // last index
         if (numberOfEntries >= capacity) {
             capacity *= 2;
-            checkCapacity (capacity); // too big ?
             list = Arrays.copyOf(list, capacity + 1);       
         }
+        
     }
 
     private void makeRoom (int newPosition){
         assert (newPosition >= 0 && newPosition <= numberOfEntries);
         for (int index = numberOfEntries; index > newPosition; index --)
             list[index] = list[index-1];
-    }
-
-	
-    public void add (T newEntry) {
-    	
-    }
+    }	
 
     public void add (int newPosition, T newEntry) {
-    	
+    	checkInitialization();
+    	makeRoom(newPosition);
+    	list[newPosition] = newEntry;
+    	numberOfEntries++;
+    	ensureCapacity();
     }
     
+    private void removeGap(int position) {
+    	for(int i=0; i < numberOfEntries-1; i++) {
+    		list[position] = list[position+1];
+    		list[numberOfEntries-1] = null;
+    	}
+    }
+    
+    //should this be a void?
     public T remove (int givenPosition) {
-    	return null;
+    	if(givenPosition < 0 || givenPosition >= numberOfEntries) {
+    		//the exception in Gia's notes, OutOfBoundariesException doesn't exist.. i think
+    		throw new IndexOutOfBoundsException();    		
+    	}
+    	T result = list[givenPosition];
+    	removeGap(givenPosition);
+    	numberOfEntries--;
+    	return result;
     }
 
-    /**
-     * Removes the specified entry from the list
-     * @param anEntry
-     * @return true if the entry was in the list; false otrherwise
-     */
-    public boolean remove ( T anEntry);
+    public boolean remove ( T anEntry) {
+    	for(int i=0; i<numberOfEntries; i++) {
+    		if(anEntry.equals(list[i])) {
+    			removeGap(i);
+    			return true;
+    		}
+    			
+    	}
+		return false;
+    }
 
-    
-    /**
-    /* Removes all entries from the list
-   */
-    public void clear();
-    
-
-
-
-
-
+    public void clear() {
+    	for(int i = 0; i < numberOfEntries; i++) {
+			list[i] = null;
+		}
+    }
 
     /**
      * Replaces an entry at given position with a new one
@@ -101,7 +116,13 @@ public class AList<T> implements ListInterface <T>   {
      * @throws IndexOutOfBoundsException if either givenPosition < 0 or
      * givenPosition >= getLength()
      */
-    public T replace (int givenPosition, T newEntry);
+    public T replace (int givenPosition, T newEntry) {
+    	if(givenPosition < 0 || givenPosition >= numberOfEntries) {
+    		throw new IndexOutOfBoundsException();
+    	}
+    	list[givenPosition] = newEntry;
+    	return newEntry;
+    };
     
     /**
      * Retrieves an entry at given position
@@ -110,7 +131,12 @@ public class AList<T> implements ListInterface <T>   {
      * @throws IndexOutOfBoundsException if either givenPosition < 0 or
      * givenPosition >= getLength()
      */
-    public T getEntry (int givenPosition);
+    public T getEntry (int givenPosition) {
+    	if(givenPosition < 0 || givenPosition >= numberOfEntries) {
+    		throw new IndexOutOfBoundsException();
+    	}
+    	return list[givenPosition];
+    }
     
    /** Retrieves all entries that are in this list in the order they occur in the list.
    /* @ return a newly allocated array of all the entries in the list
@@ -123,17 +149,31 @@ public class AList<T> implements ListInterface <T>   {
    /* @param anEntry the object: desired entry
    /* @return true if the list contains anEntry; false if not.
    */
-    public boolean contains (T anEntry);
+    public boolean contains (T anEntry) {
+    	for(int i=0; i < numberOfEntries-1; i++) {
+    		if(list[i] == anEntry) {
+    			return true;
+    		}    		
+    	}
+    	return false;
+    }
     
     /**
     /* Gets the length of this list
-    /* @return the integer number of entrieas currently in the list
+    /* @return the integer number of entries currently in the list
     */
-    public int getLength();
+    public int getLength() {
+    	return numberOfEntries;
+    }
     
     /** 
     /* Sees whether this list is empty
     /* return true if the list is empty, false if not
     */
-    public boolean isEmpty();
+    public boolean isEmpty() {
+    	if(numberOfEntries == 0) {
+    		return true;
+    	}
+    	return false;
+    }
 }
